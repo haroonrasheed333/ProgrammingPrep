e175bf22b62c37d6f6808e48ce4344240d60d5a1
Haroon Rasheed <haroonrasheed@berkeley.edu>
2014-02-12 19:16:06 -0800

    CCI-1

---
+++ CCI-1.py
@@ -0,0 +1,26 @@
+
+
+def main():
+    tstr = "paull"
+
+    intt = []
+    for i in range(0, 256):
+        intt.append(0)
+
+    n = 0
+    flag = 0
+    while n < len(tstr):
+        if intt[ord(tstr[n])] > 0:
+            flag = 1
+        else:
+            intt[ord(tstr[n])] += 1
+
+        n += 1
+
+    if flag == 0:
+        print "The characters in the string are unique"
+    else:
+        print "The characters in the string are not unique"
+
+if __name__ == '__main__':
+    main()
