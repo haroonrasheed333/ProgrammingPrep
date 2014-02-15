99a694640ddf3a64b3a25440415231a42b6f723c
rahmaniacc <rahmaniacc@gmail.com>
2014-02-15 01:32:44 -0800

    Pairs prog dfrom Hacker Rank

---
+++ Pairs_hackerrank.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+
+// you can also use imports, for example:
+// import java.math.*;
+class Solution {
+    public  static int solution(int N, int[] A) {
+        // write your code in Java SE 7
+        HashMap<Integer, Integer> lookup = new HashMap<Integer, Integer>();
+        int ret_val = 0;
+        for(int i=0;i<A.length;i++){
+            if(!lookup.containsKey(A[i])){
+                    lookup.put(A[i],A[i]-N);
+            }
+
+        }
+
+       for(int t:lookup.values()){
+           if(lookup.containsKey(t)) ret_val+=1;
+       }
+
+        return ret_val;
+    }
+
+    public static void main(String[] args) {
+        int[] t = {1,5,3,4,2 };
+        int ans = Solution.solution(2,t);
+        System.out.print(ans);
+
+    }
+}
