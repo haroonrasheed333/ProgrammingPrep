1a3e5400b1add24ec6c79696c11f38b40575e602
rahmaniacc <rahmaniacc@gmail.com>
2014-02-14 22:21:02 -0800

    JAva versison of Frog Jumping in a river

---
+++ FrogRiver.java
@@ -0,0 +1,21 @@
+// you can also use imports, for example:
+// import java.math.*;
+class Solution {
+    public int solution(int X, int[] A) {
+        // write your code in Java SE 7
+        
+        boolean[] flag_array = new boolean[X];
+        int temp_counter = 0;
+        for(int i=0; i<A.length; i++){
+            if(!flag_array[A[i]-1]){
+                
+                temp_counter++;
+                flag_array[A[i]-1] = true;
+            }
+            
+            if(temp_counter == X) return i;
+        }
+                           
+        return -1;
+    }
+}
