4c6eb2481caf81d5822a794cbca964c9278c3cff
rahmaniacc <rahmaniacc@gmail.com>
2014-02-16 01:06:54 -0800

    MAx prod of Three elemetns in an array

---
+++ MaxProdOfThree.java
@@ -0,0 +1,40 @@
+// you can also use imports, for example:
+// import java.math.*;
+class Solution {
+    public static void quickSort(int[] arr,int left,int right) {
+        int i = left, j = right;
+        int pivot = arr[(i+j) / 2];
+        while (i <= j) {
+            while (arr[i] > pivot)
+                i++;
+            while (arr[j] < pivot)
+                j--;
+
+            if (i <= j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;    
+
+            }
+        }
+
+        if (left < j)
+            quickSort(arr, left, j);
+        if (i < right)
+            quickSort(arr, i, right);
+
+
+    }
+
+    public static void main(String[] args) {
+        int[] t = {-3,1,2,-2,5,6,1};
+        Solution.quickSort(t, 0, t.length - 1);
+
+        int prod1 = t[0]*t[1]*t[2];
+        int prod2 = t[0]*t[t.length-1] *t[t.length-2];
+        System.out.println( prod1 > prod2 ? prod1:prod2);
+
+    }
+}
