114219c8a4e9a5bc9b302b561bc20379f2e56ca6
rahmaniacc <rahmaniacc@gmail.com>
2014-02-15 22:23:06 -0800

    Minimum average of slices

---
+++ MinAvgSlice.java
@@ -0,0 +1,26 @@
+// you can also use imports, for example:
+// import java.math.*;
+class Solution {
+    public int solution(int[] A) {
+        // write your code in Java SE 7
+        double min_avg = (A[0] + A[1])/2.0;
+        int min_pos = 0;
+
+        for(int i =0;i<A.length - 2; i++){
+           double two_slice_min = (A[i] + A[i+1] )/ 2.0;
+           double three_slice_min = (A[i] + A[i+1] + A[i+2]) / 3.0;
+           if(two_slice_min < min_avg){
+               min_avg  =two_slice_min;
+               min_pos = i;
+           }
+            if(three_slice_min < min_avg){
+                min_avg = three_slice_min;
+                min_pos = i;
+            }
+        }
+
+        double last_slice =(A[A.length -2] + A[A.length -1]) / 2.0;
+        if(last_slice < min_avg) return A.length - 2;
+        else return  min_pos;
+    }
+}
