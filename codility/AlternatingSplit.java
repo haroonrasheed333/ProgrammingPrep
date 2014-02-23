46dd3296cd24cf5da6609b31ca1c7f7acf083f61
Haroon Rasheed <haroonrasheed@berkeley.edu>
2014-02-22 17:57:42 -0800

    Alternate Spliting

---
+++ codility/AlternatingSplit.java
@@ -0,0 +1,52 @@
+package codility;
+
+/**
+ * Created by haroon on 2/22/14.
+ */
+public class AlternatingSplit {
+    public static IntList Solution (IntList head1) {
+        IntList curr1;
+        IntList head2, currr2;
+
+        curr1 = head1;
+        head2 = new IntList(curr1.next.value);
+        currr2 = head2;
+        currr2.next = null;
+        curr1.next = curr1.next.next;
+
+        while (curr1 != null) {
+            if (curr1.next != null) {
+                curr1 = curr1.next;
+                currr2.next = new IntList(curr1.next.value);
+                currr2 = currr2.next;
+                curr1.next = curr1.next.next;
+            }
+            else {
+                currr2.next = null;
+            }
+        }
+
+        return head2;
+    }
+
+    public static void main(String[] args) {
+        IntList number1Head = new IntList(1);
+        IntList number1 = number1Head;
+        number1.next = new IntList(2);
+        number1 = number1.next;
+        number1.next = new IntList(3);
+        number1 = number1.next;
+        number1.next = new IntList(4);
+        number1 = number1.next;
+        number1.next = new IntList(5);
+        number1 = number1.next;
+        number1.next = new IntList(6);
+        number1 = number1.next;
+        number1.next = new IntList(7);
+        number1 = number1.next;
+
+        IntList newLst = AlternatingSplit.Solution(number1Head);
+
+        System.out.println("done");
+    }
+}
