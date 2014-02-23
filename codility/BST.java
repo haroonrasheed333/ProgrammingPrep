ce5b22574bf22e7b76d128fd8eac50b24b12db1b
rahmaniacc <rahmaniacc@gmail.com>
2014-02-22 22:44:39 -0800

    Trees

---
+++ codility/BST.java
@@ -0,0 +1,110 @@
+package codility;
+
+/**
+ * Created by rahmanaicc on 2/22/14.
+ */
+public class BST {
+
+
+
+    public static Tree ConstructBST(Tree root, int A){
+        if (root == null) {
+            root = new Tree(A);
+            root.left = null;
+            root.right = null;
+        }
+        else {
+            if (A < root.value) {
+                if (root.left != null)
+                    ConstructBST(root.left, A);
+                else
+                    root.left = new Tree(A);
+            }
+            else if (A > root.value) {
+                if (root.right != null)
+                    ConstructBST(root.right, A);
+                else
+                    root.right = new Tree(A);
+            }
+        }
+
+    return root;
+
+    }
+
+    public static void inOrderTraversal(Tree root) {
+        if (root == null)
+            System.out.println("Empty BST");
+        else {
+            if (root.left != null)
+                inOrderTraversal(root.left);
+            System.out.println(root.value);
+            if (root.right != null)
+                inOrderTraversal(root.right);
+        }
+    }
+
+    public static void postOrderTraversal(Tree root) {
+        if (root == null)
+            System.out.println("Empty BST");
+        else {
+            if (root.left != null)
+                postOrderTraversal(root.left);
+            if (root.right != null)
+                postOrderTraversal(root.right);
+            System.out.println(root.value);
+        }
+    }
+
+
+    public static void preOrderTraversal(Tree root) {
+        if (root == null)
+            System.out.println("Empty BST");
+        else {
+            System.out.println(root.value);
+            if (root.left != null)
+                preOrderTraversal(root.left);
+            if (root.right != null)
+                preOrderTraversal(root.right);
+        }
+    }
+
+    public static boolean searchBST(Tree root, int k) {
+
+        while (true) {
+            if (root == null)
+                return false;
+
+            if (k == root.value)
+                return true;
+
+            if (k < root.value)
+                root = root.left;
+            else
+                root = root.right;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] A = {3,5,1,2,7,8,0,3,4,5};
+        Tree root = null;
+        for(int i = 0; i <A.length; i++) {
+            root = BST.ConstructBST(root, A[i]);
+        }
+//
+//        System.out.println("In");
+//        BST.inOrderTraversal(root);
+//        System.out.println("Post");
+//        BST.postOrderTraversal(root);
+//        System.out.println("Pre");
+//        BST.preOrderTraversal(root);
+
+        System.out.println(BST.searchBST(root, 3));
+
+        System.out.println("done");
+
+     }
+
+
+}
---
+++ codility/Tree.java
@@ -0,0 +1,17 @@
+package codility;
+
+/**
+ * Created by rahmanaicc on 2/22/14.
+ */
+public class Tree {
+    public int value;
+    public Tree left;
+    public Tree right;
+
+    Tree(int n){
+        value = n;
+        left = null;
+        right = null;
+    }
+
+}
