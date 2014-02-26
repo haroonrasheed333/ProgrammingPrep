0c9b5dcbcf296dae518b87f0423441493788bebd
Haroon Rasheed <haroonrasheed@berkeley.edu>
2014-02-26 01:48:04 -0800

    String Permutation

---
+++ codility/PermutationString.java
@@ -0,0 +1,25 @@
+package codility;
+
+/**
+ * Created by haroon on 2/26/14.
+ */
+public class PermutationString {
+    public static void main(String args[]) {
+        permuteString("", "String");
+    }
+
+    public static void permuteString(String beginningString, String endingString) {
+        if (endingString.length() <= 1)
+            System.out.println(beginningString + endingString);
+        else
+            for (int i = 0; i < endingString.length(); i++) {
+                try {
+                    String newString = endingString.substring(0, i) + endingString.substring(i + 1);
+
+                    permuteString(beginningString + endingString.charAt(i), newString);
+                } catch (StringIndexOutOfBoundsException exception) {
+                    exception.printStackTrace();
+                }
+            }
+    }
+}
